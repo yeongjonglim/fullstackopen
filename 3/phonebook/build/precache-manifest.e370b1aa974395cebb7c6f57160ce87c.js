76100cd52b1746b08ad74b038b7ad5802640a41d
Yeong Jong Lim <yeongjong.lim@gmail.com>
2020-06-06 19:15:00 +0800

    Added build folder from frontend

---
+++ 3/phonebook/build/precache-manifest.e370b1aa974395cebb7c6f57160ce87c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2acc065bce689c1eb16c753575c81d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "331afb194dc1a4d20bf6",
+    "url": "/static/css/main.aa88487c.chunk.css"
+  },
+  {
+    "revision": "e690b7896d4bc35c0816",
+    "url": "/static/js/2.ddec9da4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddec9da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "331afb194dc1a4d20bf6",
+    "url": "/static/js/main.452a4892.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
