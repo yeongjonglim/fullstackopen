65db3eeae5cb701bf6b58136a72cb2eed1b13de2
Yeong Jong Lim <yeongjong.lim@gmail.com>
2020-06-14 10:24:52 +0800

    Added routed anecdotes

---
+++ 7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    const resetField = () => {
+        setValue('')
+    }
+
+    return {
+        props: {
+            type,
+            value,
+            onChange
+        },
+        resetField
+    }
+}
